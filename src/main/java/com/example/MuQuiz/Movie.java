51e1ce2e43a52e5994d835c47cdc6ea8db1cdac2
Christoffer Deurell <christoffer.deurell@live.se>
2020-03-30 20:20:56 +0200

    .

---
+++ src/main/java/com/example/MuQuiz/Movie.java
@@ -0,0 +1,12 @@
+package com.example.MuQuiz;
+
+public class Movie {
+
+    public boolean adult;
+    String backdrop_path;
+    String belongs_to_collection;
+    public int budget;
+    public String overview;
+    public String poster_path;
+
+}
--- src/main/java/com/example/MuQuiz/startPage/startController.java
+++ src/main/java/com/example/MuQuiz/startPage/startController.java
@@ -1,13 +1,38 @@
 package com.example.MuQuiz.startPage;
 
+import com.example.MuQuiz.Movie;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
 
 @Controller
 public class startController {
+    String APIkey = "21c95e422f1845eea7a7274d9e67524b";
+    int movieId;
 
     @GetMapping("/start")
-    public String showStart(){
+    public String showStart(RestTemplate restTemplate, Model model){
+
+        movieId = 640;
+
+        Movie movie = restTemplate.getForObject("https://api.themoviedb.org/3/movie/"+movieId +"?api_key=" +APIkey +"&language=en-US", Movie.class);
+
+        model.addAttribute("overview",movie.overview);
+        model.addAttribute("url","https://image.tmdb.org/t/p/w200/" +movie.poster_path);
+
+
+        return "start";
+    }
+
+    @PostMapping("/start")
+    public String postShow(Model model, RestTemplate restTemplate, @RequestParam Integer movieId){
+        Movie movie = restTemplate.getForObject("https://api.themoviedb.org/3/movie/"+movieId +"?api_key=" +APIkey +"&language=en-US", Movie.class);
+
+        model.addAttribute("overview",movie.overview);
+        model.addAttribute("url","https://image.tmdb.org/t/p/w200/" +movie.poster_path);
 
         return "start";
     }
