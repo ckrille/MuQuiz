3aee3a9137f7c5ac2f7f59e1f60b478478724815
Christoffer Deurell <christoffer.deurell@live.se>
2020-03-30 10:46:27 +0200

    start

---
+++ src/main/java/com/example/MuQuiz/MuQuizApplication.java
@@ -0,0 +1,13 @@
+package com.example.MuQuiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuQuizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuQuizApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MuQuiz/MuQuizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MuQuiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuQuizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
