a3a862e7b27de46a11c1ae5042b10294b135903b
Christoffer Deurell <christoffer.deurell@live.se>
2020-03-31 14:28:43 +0200

    putsning i Movie och Questions

--- src/main/java/com/example/MuQuiz/questionsPage/Questions.java
+++ src/main/java/com/example/MuQuiz/questionsPage/Questions.java
@@ -10,46 +10,38 @@ import java.util.List;
 @Service
 public class Questions {
 
-   private String theQuestion;
-    List <Movie> movieList;
+    private String theQuestion;
+    private List<Movie> movieList;
+    private String APIkey = "21c95e422f1845eea7a7274d9e67524b";
+    private Long movieId;
 
-    public Questions(){
+    public Questions() {
     }
 
-    public Questions(String theQuestion, List<Movie> movieList){
+    public Questions(String theQuestion, List<Movie> movieList) {
         this.theQuestion = theQuestion;
         this.movieList = movieList;
     }
 
-    public List<Movie> getMovieList() {
-        return movieList;
-    }
-
-    public void setMovieList(List<Movie> movieList) {
-        this.movieList = movieList;
-    }
-
-    String APIkey = "21c95e422f1845eea7a7274d9e67524b";
-    int movieId;
 
     public List<Movie> questionDesc(RestTemplate restTemplate) {
-        movieId = 100;
-        List <Movie> movieList = new ArrayList<>();
+        movieId = 100L;
+        List<Movie> movieList = new ArrayList<>();
 
-        for (int i = 0; i < 3 ; i++) {
-            Movie movie = restTemplate.getForObject("https://api.themoviedb.org/3/movie/" + (movieId+i) + "?api_key=" + APIkey + "&language=en-US", Movie.class);
+        for (int i = 0; i < 3; i++) {
+            Movie movie = restTemplate.getForObject("https://api.themoviedb.org/3/movie/" + (movieId + i) + "?api_key=" + APIkey + "&language=en-US", Movie.class);
             movieList.add(movie);
         }
 
-        this.theQuestion = "Vilken film är detta? \n"+movieList.get(0).overview;
+        this.theQuestion = "Vilken film är det enligt beskrivningen? \n" + movieList.get(0).overview;
         return movieList;
     }
 
     public List<Movie> questionName(RestTemplate restTemplate) {
-        movieId = 100;
-        List <Movie> movieList = new ArrayList<>();
-        Movie movie = restTemplate.getForObject("https://api.themoviedb.org/3/movie/" + (movieId +5) +"?api_key=" + APIkey + "&language=en-US", Movie.class);
-         movieList.add(movie);
+        movieId = 100L;
+        List<Movie> movieList = new ArrayList<>();
+        Movie movie = restTemplate.getForObject("https://api.themoviedb.org/3/movie/" + (movieId + 5) + "?api_key=" + APIkey + "&language=en-US", Movie.class);
+        movieList.add(movie);
         this.theQuestion = "Vilken film är det på bilden?";
         return movieList;
     }
@@ -60,5 +52,13 @@ public class Questions {
 
     public void setTheQuestion(String theQuestion) {
         this.theQuestion = theQuestion;
+    }
+
+    public List<Movie> getMovieList() {
+        return movieList;
+    }
+
+    public void setMovieList(List<Movie> movieList) {
+        this.movieList = movieList;
     }
 }
