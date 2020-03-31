6ec0f8ebe8cef075462fa73227dc95d5b71630e1
MikaelJCarlsson <micke.c4@gmail.com>
2020-03-31 15:51:29 +0200

    Countdown timer och annat skit

--- src/main/java/com/example/MuQuiz/questionsPage/questionsController.java
+++ src/main/java/com/example/MuQuiz/questionsPage/questionsController.java
@@ -1,10 +1,13 @@
 package com.example.MuQuiz.questionsPage;
 
+        import com.example.MuQuiz.User.User;
+        import com.example.MuQuiz.User.UserRepository;
         import org.springframework.beans.factory.annotation.Autowired;
         import org.springframework.stereotype.Controller;
         import org.springframework.ui.Model;
         import org.springframework.web.bind.annotation.GetMapping;
         import org.springframework.web.bind.annotation.PostMapping;
+        import org.springframework.web.bind.annotation.RequestParam;
         import org.springframework.web.client.RestTemplate;
 
 @Controller
@@ -12,6 +15,7 @@ public class questionsController {
 
    @Autowired
    Questions questions;
+
 
 
 
@@ -26,9 +30,11 @@ public class questionsController {
     }
 
     @PostMapping("/questions")
-    public String postShow(RestTemplate restTemplate,Model model){
+    public String postShow(RestTemplate restTemplate, Model model){
         Questions qu = new Questions();
+        User u = new User();
 
+        model.addAttribute("username",u.getUsername());
         model.addAttribute("url",qu.questionName(restTemplate));
         model.addAttribute("overview",qu.getTheQuestion());
         return "questions";
