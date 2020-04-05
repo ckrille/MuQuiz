060fa49cf8800103c6a8e04e3251c784fe5fb55a
Daniel <danielmelker@gmail.com>
2020-04-05 23:08:26 +0200

    css

--- src/main/java/com/example/MuQuiz/MuQuizApplication.java
+++ src/main/java/com/example/MuQuiz/MuQuizApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MuQuizApplication {
+public class  MuQuizApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MuQuizApplication.class, args);
